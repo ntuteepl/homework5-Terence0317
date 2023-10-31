f25e0e04e7d11d0afa77a629f2109e1555b63671
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-31 13:41:07 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int min;
+    float cost;
+    scanf("%d", &min);
+    if (min >= 0 && min <= 800) cost = 0.9 * min;
+    else if (min > 800 && min < 1500) cost = 0.81 * min;
+    else cost = 0.6399 * min;
+    printf("%.1lf", cost);
+}
