8179f4ecd10287d04c6a4c24398472724d945161
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-31 14:39:30 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int h, m;
+    float angle, angleh, dah, anglem;
+    scanf("%d%d", &h, &m);
+    angleh = h * 30 + 0.5 * m;
+    anglem = m * 6 ;
+    angle = angleh - anglem;
+    if (angle >= 180) printf("%1lf", angle-180);
+    else printf("%1lf", angle);
+}
