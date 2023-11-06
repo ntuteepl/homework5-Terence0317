43ac6bfe5be8edc93d79851a4090111c0b011d33
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-11-07 02:30:38 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -6,11 +6,11 @@ int main()
     int h, m;
     float angle, angleh, dah, anglem;
     scanf("%d%d", &h, &m);
-    if (h==12) angleh = 0.5 * m;
-    else angleh = h * 30 + 0.5 * m;
+    if (h == 12) angleh = 0.5 * m;
+    else angleh = (h * 30 + 0.5 * m);
     anglem = m * 6 ;
     angle = angleh - anglem;
-    if (angle >= 180) printf("%.3lf", angle-180);
+    if (angle >= 180) printf("%.3lf", 360 - angle);
     else if (angle < 0) printf("%.3lf", angle + (-2)*angle);
     else printf("%.3lf", angle);
 }
